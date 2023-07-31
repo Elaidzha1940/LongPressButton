d6ddc360391cb19c94a123eaa4f47a2054efc9df
Elaidzha1940 <eli080293@gmail.com>
2023-07-31 18:51:54 +0300

    LongPressButton

--- LongPress Button/LongPress Button/ContentView.swift
+++ LongPress Button/LongPress Button/ContentView.swift
@@ -19,22 +19,22 @@ struct ContentView: View {
         
         ZStack {
             Color.gray.opacity(0.1)
-
+            
             ZStack {
                 Circle()
-                    .frame(width: 120, height: 120)
+                    .frame(width: 130, height: 130)
                     .foregroundColor(.gray.opacity(0.1))
                     .shadow(color: .black.opacity(0.1), radius: 10, x: 10, y: 10)
                     .shadow(color: .white.opacity(0.6), radius: 10, x: -20, y: -20)
                 
                 Circle()
                     .stroke(lineWidth: 7)
-                    .frame(width: 120, height: 120)
+                    .frame(width: 130, height: 130)
                     .foregroundColor(Color.gray.opacity(0.1))
                 
                 Circle()
                     .stroke(lineWidth: 5.5)
-                    .frame(width: 105, height: 105)
+                    .frame(width: 115, height: 115)
                     .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.06), Color.gray.opacity(0.01), Color.black.opacity(0.06)]), startPoint: .topLeading, endPoint: .bottomTrailing))
                 
                 if topG {
@@ -44,7 +44,25 @@ struct ContentView: View {
                         .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.01), .white]), startPoint: .topLeading, endPoint: .bottomTrailing))
                         .blur(radius: 3)
                 }
+                ZStack {
+                    Circle()
+                        .frame(width: 90, height: 90)
+                        .foregroundColor(.white)
+                    Image("print")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 90, height: 90)
+                        .cornerRadius(50)
+                }
+                
             }
+            .gesture(LongPressGesture().updating($topG) {cstate, gstate, transition in
+                gstate = cstate
+            }
+                .onEnded({ value in
+                    HPress.toggle()
+                })
+            )
         }
         .ignoresSafeArea()
     }
