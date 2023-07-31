0d494d852294f24d1cb58f2a179ba2fc36ca2ced
Elaidzha1940 <eli080293@gmail.com>
2023-07-31 18:14:06 +0300

    LongPressButton

--- LongPress Button/LongPress Button/ContentView.swift
+++ LongPress Button/LongPress Button/ContentView.swift
@@ -12,15 +12,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-            
-            ZStack {
-                Color.red
-            }
-            
-            VStack {
-                
-            }
+        ZStack {
+            Color.gray.opacity(0.2)
+            Circle()
+                .frame(width: 120, height: 120)
+                .foregroundColor(.white.opacity(0.6))
+                .shadow(color: .black.opacity(0.1), radius: 10, x: 10, y: 10)
+                .shadow(color: .white.opacity(0.3), radius: 10, x: -20, y: -20)
+            Circle()
+                .frame(width: 120, height: 120)
+                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.06), Color.gray.opacity(0.01), Color.black.opacity(0.06)]), startPoint: .topLeading, endPoint: .bottomTrailing))
         }
         .ignoresSafeArea()
     }
