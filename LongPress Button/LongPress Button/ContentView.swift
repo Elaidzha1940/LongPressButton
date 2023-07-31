593be5681eda271ba76dfa4ecbb0e2c9196b2e15
Elaidzha1940 <eli080293@gmail.com>
2023-07-31 18:30:36 +0300

    LongPressButton

--- LongPress Button/LongPress Button/ContentView.swift
+++ LongPress Button/LongPress Button/ContentView.swift
@@ -10,31 +10,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var HPress = false
+    @State var FB = false
+    @GestureState var topG = false
+    
     var body: some View {
         
         ZStack {
             Color.gray.opacity(0.1)
-            Circle()
-                .frame(width: 120, height: 120)
-                .foregroundColor(.gray.opacity(0.1))
-                .shadow(color: .black.opacity(0.1), radius: 10, x: 10, y: 10)
-                .shadow(color: .white.opacity(0.6), radius: 10, x: -20, y: -20)
-            
-            Circle()
-                .stroke(lineWidth: 7)
-                .frame(width: 120, height: 120)
-                .foregroundColor(Color.gray.opacity(0.1))
-            
-            Circle()
-                .stroke(lineWidth: 5.5)
-                .frame(width: 105, height: 105)
-                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.06), Color.gray.opacity(0.01), Color.black.opacity(0.06)]), startPoint: .topLeading, endPoint: .bottomTrailing))
-            
-            Circle()
-                .stroke(lineWidth: 15)
-                .frame(width: 105, height: 105)
-                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.01), .white]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                .blur(radius: 3)
+
+            ZStack {
+                Circle()
+                    .frame(width: 120, height: 120)
+                    .foregroundColor(.gray.opacity(0.1))
+                    .shadow(color: .black.opacity(0.1), radius: 10, x: 10, y: 10)
+                    .shadow(color: .white.opacity(0.6), radius: 10, x: -20, y: -20)
+                
+                Circle()
+                    .stroke(lineWidth: 7)
+                    .frame(width: 120, height: 120)
+                    .foregroundColor(Color.gray.opacity(0.1))
+                
+                Circle()
+                    .stroke(lineWidth: 5.5)
+                    .frame(width: 105, height: 105)
+                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.06), Color.gray.opacity(0.01), Color.black.opacity(0.06)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                
+                if topG {
+                    Circle()
+                        .stroke(lineWidth: 15)
+                        .frame(width: 105, height: 105)
+                        .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.01), .white]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .blur(radius: 3)
+                }
+            }
         }
         .ignoresSafeArea()
     }
