5fb5b744868d518cc63c7f71e85eebff6ee276ea
Elaidzha1940 <eli080293@gmail.com>
2023-07-31 17:54:22 +0300

    LongPressButton

---
+++ LongPress Button/LongPress Button/ContentView.swift
@@ -0,0 +1,33 @@
+//  /*
+//
+//  Project: LongPress Button
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date:
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+            ZStack {
+                Color.red
+            }
+            
+            VStack {
+                
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LongPress Button/LongPress Button/LongPress_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongPress_ButtonApp.swift
+//  LongPress Button
+//
+//  Created by Elaidzha Shchukin on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct LongPress_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
